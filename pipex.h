3ca0a1d3bd8e5b88d9fc89e955229b8384b98341
Royston Lee <roylee@c2r4s3.42singapore.sg>
2023-10-08 15:27:55 +0800

    add headers

---
+++ pipex.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roylee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/08 13:30:43 by roylee            #+#    #+#             */
+/*   Updated: 2023/10/08 14:21:11 by roylee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <sys/types.h> // fork()
+# include <sys/wait.h> // wait, waitpid
+# include <errno.h>
+# include <fcntl.h>
+
+# include "libft/libft.h"
+
+#endif
