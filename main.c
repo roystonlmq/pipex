a8ad32a4f5610d50e7ce9566a067a6042493487f
Royston Lee <roylee@c2r4s3.42singapore.sg>
2023-10-14 11:41:55 +0800

    update struct

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: roylee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 14:13:24 by roylee            #+#    #+#             */
-/*   Updated: 2023/10/08 17:44:24 by roylee           ###   ########.fr       */
+/*   Updated: 2023/10/11 20:19:22 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,13 @@ int	main(int argc, char **argv, char **envp)
 	int		fd[2];
 	pid_t	childpid;
 
-	pipe(fd);
-	childpid = fork();
-	(void)	childpid;	
-	(void)	argv;
-	(void)	envp;
 	if (argc != 5)
 	{
 		ft_putstr_fd("Incorrect num of args\n", 2);
 		ft_putstr_fd("Ex: ./pipex <file1> <cmd1> <cmd2> <file2>\n", 1);
 		exit(1);
 	}
-		
+	pipe(fd);
+	childpid = fork();
+	if 		
 }
--- pipex.h
+++ pipex.h
@@ -6,18 +6,40 @@
 /*   By: roylee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 13:30:43 by roylee            #+#    #+#             */
-/*   Updated: 2023/10/08 14:21:11 by roylee           ###   ########.fr       */
+/*   Updated: 2023/10/14 11:33:16 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include <sys/types.h> // fork()
+# include <sys/types.h> // fork
 # include <sys/wait.h> // wait, waitpid
-# include <errno.h>
-# include <fcntl.h>
+# include <string.h> // strerror
+# include <fcntl.h> // open, unlink
+# include <stdio.h> // perror
+# include <stdlib.h> // malloc, free, exit
+# include <unistd.h> // read, write, close, access, dup, dup2, execve, pipe
+# include "../GNL/get_next_line.h" // gnl func
 
-# include "libft/libft.h"
+# define INFILE "Infile"
+# define OUTFILE "Outfile"
+# define PIPE "Pipe"
+# define ARGS_ERR "Num of args should be 5"
+# define CMD_ERR "Invalid cmd\n"
+
+
+typedef struct s_pipe
+{
+	pid_t	pid1;
+	pid_t	pid2;
+	int		pipe[2];
+	int		in_f;
+	int		out_f;
+	char	*paths;
+	char	**cmd_paths;
+	char	**cmd_args;
+}
+
 
 #endif
