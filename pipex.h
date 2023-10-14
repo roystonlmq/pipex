1ec3339e521a1853b3c73666034d394657e080a1
Royston Lee <roylee@c2r4s3.42singapore.sg>
2023-10-14 14:01:31 +0800

    adding constants, updating funcs

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 13:30:43 by roylee            #+#    #+#             */
-/*   Updated: 2023/10/14 12:48:14 by roylee           ###   ########.fr       */
+/*   Updated: 2023/10/14 13:41:58 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@
 # define INFILE "Infile"
 # define OUTFILE "Outfile"
 # define PIPE "Pipe"
+# define ENV_ERR "Invalid environment"
 # define ARGS_ERR "Num of args should be 5"
 # define CMD_ERR "Invalid cmd\n"
 
@@ -36,6 +37,11 @@ Argument index for infd, outfd
 # define INFD 1
 # define OUTFD 4
 
+/*
+CONSTS to define read and write end of pipe
+*/
+# define READ 0
+# define WRITE 1
 
 typedef struct s_pipe
 {
@@ -44,10 +50,21 @@ typedef struct s_pipe
 	int		pipe[2];
 	int		infd;
 	int		outfd;
-	char	*paths;
+	char	*envpaths;
 	char	**cmd_paths;
 	char	**cmd_args;
 }	t_pipe;
 
+/*
+cleaner.c
+*/
+void	close_all(t_pipe *piper);
+void	close_parent(t_pipe *piper);
+void	close_child(t_pipe *piper);
 
+/*
+error_handler.c
+*/
+int		putstr_fd(int fd, char *s);
+void	exception(char *error);
 #endif
