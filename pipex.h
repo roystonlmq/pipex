9943dcb6dcced81e75defa232cdc86627aa41830
Royston Lee <roylee@c2r4s3.42singapore.sg>
2023-10-14 12:48:43 +0800

    add INFD, OUTFD consts

--- pipex.h
+++ pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: roylee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 13:30:43 by roylee            #+#    #+#             */
-/*   Updated: 2023/10/14 11:33:16 by roylee           ###   ########.fr       */
+/*   Updated: 2023/10/14 12:48:14 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,18 +28,26 @@
 # define ARGS_ERR "Num of args should be 5"
 # define CMD_ERR "Invalid cmd\n"
 
+/* 
+Argument index for infd, outfd
+./pipex file1 cmd1 cmd2 file2
+0		1	  2	   3    4
+*/
+# define INFD 1
+# define OUTFD 4
+
 
 typedef struct s_pipe
 {
 	pid_t	pid1;
 	pid_t	pid2;
 	int		pipe[2];
-	int		in_f;
-	int		out_f;
+	int		infd;
+	int		outfd;
 	char	*paths;
 	char	**cmd_paths;
 	char	**cmd_args;
-}
+}	t_pipe;
 
 
 #endif
