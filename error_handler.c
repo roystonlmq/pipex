2d2f253e26f8ca039b4ddcac0dfe000cd7ca89b6
Royston Lee <roylee@c2r4s3.42singapore.sg>
2023-10-14 14:13:15 +0800

    change fn name

--- error_handler.c
+++ error_handler.c
@@ -6,13 +6,13 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 12:10:55 by roylee            #+#    #+#             */
-/*   Updated: 2023/10/14 12:37:38 by roylee           ###   ########.fr       */
+/*   Updated: 2023/10/14 14:13:00 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
 
-int	putstr_fd(int fd, char *s)
+int	ft_putstr_fd(int fd, char *s)
 {
 	write(fd, s, ft_strlen(s));
 	return (ft_strlen(s));
