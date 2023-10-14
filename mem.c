cc1f390d4a35ef94ae9f4e57afd2f6de2be171bf
Royston Lee <roylee@c2r4s3.42singapore.sg>
2023-10-14 14:03:55 +0800

    add more mem funcs

---
+++ mem.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mem.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 13:58:07 by roylee            #+#    #+#             */
+/*   Updated: 2023/10/14 14:03:45 by roylee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*dest_cpy;
+	unsigned char	*src_cpy;
+	size_t			i;
+
+	if (!dest && !src)
+		return (0);
+	dest_cpy = (unsigned char *)dest;
+	src_cpy = (unsigned char *)src;
+	i = 0;
+	while (n > 0 && i < n)
+	{
+		dest_cpy[i] = src_cpy[i];
+		i++;
+	}
+	return (dest);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*buffer;
+
+	buffer = (void *)malloc(nmemb * size);
+	if (!buffer)
+		return (NULL);
+	ft_bzero(buffer, nmemb * size);
+	return (buffer);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, 0, n);
+}
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)s;
+	while (n > 0)
+	{
+		*(ptr++) = (unsigned char)c;
+		n--;
+	}
+	return (s);
+}
