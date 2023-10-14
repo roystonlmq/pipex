f0dec0f76e0702916c59f656d683f2de6f83a58c
Royston Lee <roylee@c2r4s3.42singapore.sg>
2023-10-14 14:26:44 +0800

    remove bzero, etc. not needed

--- mem.c
+++ mem.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 13:58:07 by roylee            #+#    #+#             */
-/*   Updated: 2023/10/14 14:14:16 by roylee           ###   ########.fr       */
+/*   Updated: 2023/10/14 14:23:04 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,33 +29,4 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 		i++;
 	}
 	return (dest);
-}
-
-void	*ft_calloc(size_t nmemb, size_t size)
-{
-	void	*buffer;
-
-	buffer = (void *)malloc(nmemb * size);
-	if (!buffer)
-		return (NULL);
-	ft_bzero(buffer, nmemb * size);
-	return (buffer);
-}
-
-void	ft_bzero(void *s, size_t n)
-{
-	ft_memset(s, 0, n);
-}
-
-void	*ft_memset(void *s, int c, size_t n)
-{
-	unsigned char	*ptr;
-
-	ptr = (unsigned char *)s;
-	while (n > 0)
-	{
-		*(ptr++) = (unsigned char)c;
-		n--;
-	}
-	return (s);
 }
