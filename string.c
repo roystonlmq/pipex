3a06c9b09ca89fe4fbaee4081bc42fea0a64626a
Royston Lee <roylee@c2r4s1.42singapore.sg>
2023-10-15 14:29:11 +0800

    strlcpy wrong order

--- string.c
+++ string.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 13:40:10 by roylee            #+#    #+#             */
-/*   Updated: 2023/10/14 14:24:17 by roylee           ###   ########.fr       */
+/*   Updated: 2023/10/15 14:24:44 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,6 +71,25 @@ char	*ft_strjoin(const char *s1, const char *s2)
 }
 
 /*
+ft_strlcpy:
+Copies src to dst up to size - 1 bytes then null-terminates it.
+*/
+static size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	slen;
+
+	slen = ft_strlen(src);
+	if (size > slen + 1)
+		ft_memcpy(dst, src, slen + 1);
+	else if (size != 0)
+	{
+		ft_memcpy(dst, src, size - 1);
+		dst[size - 1] = 0;
+	}
+	return (slen);
+}
+
+/*
 ft_substr:
 Used to locate the substring in an array of strings
 */
@@ -89,23 +108,4 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 		return (NULL);
 	ft_strlcpy(buffer, s + start, len + 1);
 	return (buffer);
-}
-
-/*
-ft_strlcpy:
-Copies src to dst up to size - 1 bytes then null-terminates it.
-*/
-static size_t	ft_strlcpy(char *dst, const char *src, size_t size)
-{
-	size_t	slen;
-
-	slen = ft_strlen(src);
-	if (size > slen + 1)
-		ft_memcpy(dst, src, slen + 1);
-	else if (size != 0)
-	{
-		ft_memcpy(dst, src, size - 1);
-		dst[size - 1] = 0;
-	}
-	return (slen);
 }
