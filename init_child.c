af3e4e1096d951ee2f78223ac85bc8c11b826867
roylee <roylee@student.42singapore.sg>
2023-10-21 14:58:40 +0800

    close_pipe func integrated into child funcs

--- init_child.c
+++ init_child.c
@@ -34,19 +34,9 @@ static char	*cmd(char **path_args, char *name)
 	return (0);
 }
 
-/*
-child_reader: Responsible for reading from pipe
-
-Duplicate the read end of the pipe to STDIN & closes write end of the pipe
-Write end is not used, so it is closed. 
-*/
-void	child_reader(t_pipe piper, char **argv, char **envp)
+void	execute_args(t_pipe piper, char **argv, char **envp, int cmd_idx)
 {
-	dup2(piper.pipe[READ], STDIN_FILENO);
-	close(piper.pipe[READ]);
-	close(piper.pipe[WRITE]);
-	dup2(piper.outfd, STDOUT_FILENO);
-	piper.cmd_args = ft_split(argv[3], ' ');
+	piper.cmd_args = ft_split(argv[cmd_idx], ' ');
 	piper.cmd = cmd(piper.cmd_paths, piper.cmd_args[0]);
 	if (!piper.cmd)
 	{
@@ -55,26 +45,47 @@ void	child_reader(t_pipe piper, char **argv, char **envp)
 	}
 	execve(piper.cmd, piper.cmd_args, envp);
 }
+/*
+Pipe overview
+
+(STDIN) ls -l (OUTFD/WRITE) =======> (INFD/READ) wc -l (STDOUT)
+
+child_reader: Responsible for reading from pipe
+
+Responsible for handling CMD2. Duplicates the read end of the pipe 
+to STDIN. 
+Write end of the pipe is not used, so it is closed. 
+Duplicates the output fd into STDOUT.
+*/
+void	child_reader(t_pipe piper, char **argv, char **envp, int cmd_idx)
+{
+	dup2(piper.pipe[READ], STDIN_FILENO);
+	close_pipe(piper);
+	dup2(piper.outfd, STDOUT_FILENO);
+	execute_args(piper, argv, envp, cmd_idx);
+}
 
 /*
+Pipe overview
+t ttesteststsetstsetsttststststststststststststes
+
+(STDIN) ls -l (OUT FD/WRITE) =======> (IN FD/READ) wc -l (STDOUT)
+
 child_writer: Responsible for writing from pipe
 
-Duplicate the write end of the pipe to STDOUT & closes read end of the pipe
+
+Responsible for handling CMD1 because CMD1's output is written to
+the pipe (infd)
+Duplicate the write end of the pipe to STDOUT.
 Read end is not used so it is closed.
+Duplicates the input fd into STDIN.
+
 Ensures any attempt to read from the pipe will result in EOF condition
 */
-void	child_writer(t_pipe piper, char **argv, char **envp)
+void	child_writer(t_pipe piper, char **argv, char **envp, int cmd_idx)
 {
 	dup2(piper.pipe[WRITE], STDOUT_FILENO);
-	close(piper.pipe[WRITE]);
-	close(piper.pipe[READ]);
+	close_pipe(piper);
 	dup2(piper.infd, STDIN_FILENO);
-	piper.cmd_args = ft_split(argv[2], ' ');
-	piper.cmd = cmd(piper.cmd_paths, piper.cmd_args[0]);
-	if (!piper.cmd)
-	{
-		close_child(&piper);
-		exception(CMD_ERR);
-	}
-	execve(piper.cmd, piper.cmd_args, envp);
+	execute_args(piper, argv, envp, cmd_idx);
 }
