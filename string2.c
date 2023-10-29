4c2f4cef2fe8215c7167755d65c5dbdb36ef0e58
roylee <roystonlmq@gmail.com>
2023-10-29 18:39:49 +0800

    add strchr

--- string2.c
+++ string2.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 14:13:59 by roylee            #+#    #+#             */
-/*   Updated: 2023/10/14 14:14:08 by roylee           ###   ########.fr       */
+/*   Updated: 2023/10/29 18:21:45 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,17 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (s1[i] == s2[i] && s1[i] && s2[i] && i < (n - 1))
 		i++;
 	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+}
+
+/*
+ft_strchr:
+Looks for a char match
+*/
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s && *s != (unsigned char)c)
+		s++;
+	if (*s == (unsigned char)c)
+		return ((char *)s);
+	return (0);
 }
