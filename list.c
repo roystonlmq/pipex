bf741521d03c18c503ac112405fd95a01db6acbd
roylee <roystonlmq@gmail.com>
2023-10-29 18:41:21 +0800

    add function to find lstsize and convert lst to arr

--- list.c
+++ list.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 23:57:02 by roylee            #+#    #+#             */
-/*   Updated: 2023/10/29 05:07:56 by roylee           ###   ########.fr       */
+/*   Updated: 2023/10/29 18:37:43 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,43 @@ t_list	*ft_lstlast(t_list *lst)
 	return (lst);
 }
 
-t_list	*ft_lstadd_back(t_list **lst, t_list *new)
+int	ft_lstsize(t_list *lst)
 {
-	if (!lst || !new)
+	int	i;
+
+	i = 0;
+	if (!lst)
+		return (0);
+	while (lst)
+	{
+		lst = lst->next;
+		i++;
+	}
+	return (i);
+}
+
+char	**lst_to_strarr(t_list *lst)
+{
+	int		arrlen;
+	int		i;
+	t_list	*tmp;
+	char	**array;
+
+	arrlen = ft_lstsize(lst);
+	array = malloc(sizeof(char *) * (arrlen + 1));
+	if (!array)
+	{
+		free(array);
 		return (NULL);
-	if (*lst)
-		ft_lstlast(*lst)->next = new;
-	else
-		*lst = new;
+	}
+	array[arrlen] = NULL;
+	i = 0;
+	while (i < arrlen)
+	{
+		array[i++] = (char *)lst->content;
+		tmp = lst->next;
+		free(lst);
+		lst = tmp;
+	}
+	return (array);
 }
